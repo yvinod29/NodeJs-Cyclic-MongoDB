edec028d5aa17a965682b1052e6af09fc2696161
yvinod29 <vinuureddy555@gmail.com>
2023-08-24 10:57:48 +0530

    init

--- models/book.js
+++ models/book.js
@@ -1,15 +1,10 @@
-const  mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const Schema=mongoose.Schema;
-const BookSchema=new Schema({
-    title:{
-        type:String,
-        required:true,
+const bookSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+});
 
-    },
-    body:{
-        type:String,
-        require:true,
-    }
-})
-mongoose.exports =mongoose.model('Book',BookSchema);
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
